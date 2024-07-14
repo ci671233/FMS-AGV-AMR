c8a639bdcaf86aa34dbfee241c74acc845635126
changin99 <20217140@edu.hanbat.ac.kr>
2024-07-14 16:42:21 +0900

    control_server

--- control_server/src/routes/control.route.js
+++ control_server/src/routes/control.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controlController = require('../controllers/control.controller');
-const authMiddleware = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
-router.post('/control/:id', authMiddleware.authenticate, controlController.controlRobot);
+router.post('/control/:id', controlController.controlRobot);
 
 module.exports = router;
 
